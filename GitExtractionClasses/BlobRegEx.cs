f00396300e53600ffcd0b5aaf81c3cd62c653a2b
Mark Howard <markjameshoward@hotmail.co.uk>
2024-11-17 12:05:54 +1300

    refactor blob

---
+++ GitExtractionClasses/BlobRegEx.cs
@@ -0,0 +1,39 @@
+public class BlobNodeExtraction
+{
+    public void ProcessBlob(string treeHash, CommitNodeExtraction CommitNode)
+    {
+        // Get the details of the Blobs in this Tree
+        string tree = FileType.GetContents(CommitNode.commitTreeDetails.Groups[1].Value, GlobalVars.workingArea);
+
+        var blobsInTree = Regex.Matches(tree, @"blob ([0-9a-f]{4})[0-9a-f]{36}.([\w\.]+)");
+
+        foreach (Match blobMatch in blobsInTree)
+        {
+            string blobHash = blobMatch.Groups[1].Value;
+            string blobContents = string.Empty;
+
+            if (GlobalVars.PerformTextExtraction)
+            {
+                FileType.GetContents(blobHash, GlobalVars.workingArea);
+            }
+
+            //Console.WriteLine($"\t\t-> blob {blobHash} {blobMatch.Groups[2]}");
+            if (GlobalVars.EmitNeo && !FileType.DoesNodeExistAlready(Neo4jHelper.session, blobHash, "blob"))
+            {
+                if (GlobalVars.EmitNeo)
+                    BlobCode.AddBlobToNeo(Neo4jHelper.session, blobMatch.Groups[2].Value, blobMatch.Groups[1].Value, blobContents);
+            }
+            //Console.WriteLine($"Adding non orphan blob {blobMatch.Groups[1].Value}");
+
+            BlobCode.AddToBlobObjectCollection(treeHash, blobMatch.Groups[2].Value, blobMatch.Groups[1].Value, blobContents);
+
+            if (GlobalVars.EmitNeo && !Links.DoesTreeToBlobLinkExist(Neo4jHelper.session, CommitNode.commitTreeDetails.Groups[1].Value, blobHash))
+            {
+                if (GlobalVars.EmitNeo)
+                    Neo4jHelper.CreateLinkNeo(Neo4jHelper.session, CommitNode.commitTreeDetails.Groups[1].Value, blobMatch.Groups[1].Value, "", "");
+            }
+
+            TreeNodesList.CreateTreeToBlobLinkJson(CommitNode.commitTreeDetails.Groups[1].Value, blobMatch.Groups[1].Value);
+        }
+    }
+}
--- GitRepoExaminer.cs
+++ GitRepoExaminer.cs
@@ -45,7 +45,6 @@ public abstract class GitRepoExaminer
                         {
                             // Get details of the tree,parent and comment in this commit
                             string treeHash = CommitNode.commitTreeDetails.Groups[1].Value;
-                            //Console.WriteLine($"\t-> tree {treeHash}");
 
                             List<string> commitParentHashes = new List<string>();
 
@@ -78,38 +77,8 @@ public abstract class GitRepoExaminer
                                 Neo4jHelper.CreateCommitLinkNeo(Neo4jHelper.session, hashCode, treeHash, "", "");
                             }
 
-                            // Get the details of the BlobCode.Blobs in this Tree
-                            string tree = FileType.GetContents(CommitNode.commitTreeDetails.Groups[1].Value, GlobalVars.workingArea);
-                            var blobsInTree = Regex.Matches(tree, @"blob ([0-9a-f]{4})[0-9a-f]{36}.([\w\.]+)");
-
-                            foreach (Match blobMatch in blobsInTree)
-                            {
-                                string blobHash = blobMatch.Groups[1].Value;
-                                string blobContents = string.Empty;
-
-                                if (GlobalVars.PerformTextExtraction)
-                                {
-                                    FileType.GetContents(blobHash, GlobalVars.workingArea);
-                                }
-
-                                //Console.WriteLine($"\t\t-> blob {blobHash} {blobMatch.Groups[2]}");
-                                if (GlobalVars.EmitNeo && !FileType.DoesNodeExistAlready(Neo4jHelper.session, blobHash, "blob"))
-                                {
-                                    if (GlobalVars.EmitNeo)
-                                        BlobCode.AddBlobToNeo(Neo4jHelper.session, blobMatch.Groups[2].Value, blobMatch.Groups[1].Value, blobContents);
-                                }
-                                //Console.WriteLine($"Adding non orphan blob {blobMatch.Groups[1].Value}");
-
-                                BlobCode.AddToBlobObjectCollection(treeHash, blobMatch.Groups[2].Value, blobMatch.Groups[1].Value, blobContents);
-
-                                if (GlobalVars.EmitNeo && !Links.DoesTreeToBlobLinkExist(Neo4jHelper.session, CommitNode.commitTreeDetails.Groups[1].Value, blobHash))
-                                {
-                                    if (GlobalVars.EmitNeo)
-                                        Neo4jHelper.CreateLinkNeo(Neo4jHelper.session, CommitNode.commitTreeDetails.Groups[1].Value, blobMatch.Groups[1].Value, "", "");
-                                }
-
-                                TreeNodesList.CreateTreeToBlobLinkJson(CommitNode.commitTreeDetails.Groups[1].Value, blobMatch.Groups[1].Value);
-                            }
+                            BlobNodeExtraction BlobNode = new();
+                            BlobNode.ProcessBlob(treeHash, CommitNode);
                         }
                         else
                         {
